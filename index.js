a690b976df7d2cdaa5760fcd519cb6b59572dc66
ashiq22 <83548540+ashiq22@users.noreply.github.com>
2024-08-20 13:39:46 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const http = require('http');
+const { DefaultAzureCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
+
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+// Set your Key Vault URL and secret name
+const keyVaultName ="msbpro-kv-prod-01"
+const kvUri = `https://${keyVaultName}.vault.azure.net`;
+const secretName ="myname"
+
+// Create a new secret client using the DefaultAzureCredential
+const credential = new DefaultAzureCredential();
+const client = new SecretClient(kvUri, credential);
+
+// Fetch the secret asynchronously
+async function getSecret() {
+  try {
+    const secret = await client.getSecret(secretName);
+    return secret.value;
+  } catch (err) {
+    console.error(`Error fetching secret: ${err}`);
+    return null;
+  }
+}
+
+const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const secretValue = await getSecret();
+  const msg = secretValue ? `Hello Node! Secret Value: ${secretValue}\n` : 'Hello Node! Could not retrieve secret.\n';
   res.end(msg);
 });
 
